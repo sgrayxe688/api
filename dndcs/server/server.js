1acb10c0cde23f73fdac659bfc5edd8fadaf2c09
Ethan Gray <ethangray505@gmail.com>
2022-06-20 23:23:18 -0500

    Connects to MongoDB

--- dndcs/server/server.js
+++ dndcs/server/server.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongodb");
+});
 
 app.use(morgan("tiny"));
 
